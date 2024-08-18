563a9bba80b1eca4e343245f16660e55bfb16c05
AlexFerreira10 <correaferreiraalex92@email.com>
2024-08-18 16:51:55 -0300

    Refactoring the User entity.

--- src/main/java/com/alexferreira10/demo/model/entities/user/User.java
+++ src/main/java/com/alexferreira10/demo/model/entities/user/User.java
@@ -1,99 +1,65 @@
 package com.alexferreira10.demo.model.entities.user;
 
-import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "User")
 @Table(name = "tb_user")
-//@Getter
-//@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-
-public class User implements Serializable, UserDetails {
-	//UserDetails: Control atribute class
-	private static final long serialVersionUID = 1L;
+public class User implements UserDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+
+	@Column(name = "login")
 	private String login;
+
+	@Column(name = "password")
 	private String password;
-	
-	public Long getId() {
-		return id;
-	}	
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getLogin() {
-		return login;
-	}
-	
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	
-	
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	//Profile access control, study after
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+		return List.of();
 	}
-	
+
+	@Override
 	public String getPassword() {
 		return password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return login;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
+		return UserDetails.super.isAccountNonExpired();
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
+		return UserDetails.super.isAccountNonLocked();
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
+		return UserDetails.super.isCredentialsNonExpired();
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
+		return UserDetails.super.isEnabled();
 	}
 }
